93a11601b8693e285bf9504de5d7fea82f06ad9a
Lyle Hanson <lyle.hanson@wisc.edu>
2018-04-01 14:44:26 -0500

    Enable completion for shell aliases

---
+++ bash/.config/bash/interactive.d/22-complete-aliases.bash
@@ -0,0 +1,9 @@
+################################################################################
+# complete-aliases.bash - Specify aliases to be subject to shell completion.
+#
+# Depends on complete-alias per https://github.com/cykerway/complete-alias
+# Execution of specific alias completion follows in a different file.
+################################################################################
+
+# git
+complete -F _complete_alias g
