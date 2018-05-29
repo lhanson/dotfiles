81e4a27f8ba9a229cc2f9e3c653a0ccdba7b4112
Lyle Hanson <lyle.hanson@wisc.edu>
2018-05-29 17:03:28 -0500

    Running polybar on all connected monitors

--- polybar/.config/polybar/launch.sh
+++ polybar/.config/polybar/launch.sh
@@ -6,6 +6,9 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-polybar --config=$HOME/.config/polybar/config top &
+# Run the bar on each monitor
+for m in $(polybar --list-monitors | cut -d":" -f1); do
+	MONITOR=$m polybar --config=$HOME/.config/polybar/config top &
+done
 
 echo "Bars launched..."
