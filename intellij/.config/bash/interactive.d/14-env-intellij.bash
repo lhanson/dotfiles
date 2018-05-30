52350a9ac544713b342856d9985ee597f3a54872
Lyle Hanson <lyle.hanson@wisc.edu>
2018-05-30 16:18:00 -0500

    Add properties for IntelliJ IDEA

---
+++ intellij/.config/bash/interactive.d/14-env-intellij.bash
@@ -0,0 +1,5 @@
+################################################################################
+# env-intellij.bash - environment variables for IntelliJ IDEA
+################################################################################
+
+export IDEA_PROPERTIES=${XDG_CONFIG_HOME}/intellij/idea.properties
